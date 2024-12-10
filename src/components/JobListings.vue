a15aeaa993c774067015036e3b91763f5150fb27
Ibrahim Bensaadoune <bensaadouneibrahim@gmail.com>
2024-12-10 20:40:22 +0100

    Update JobListings.vue
    Get Jobs Data using axios and add spinner

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,12 +1,55 @@
+<script setup>
+import { reactive, defineProps, onMounted } from "vue";
+import { RouterLink } from "vue-router";
+import JobListing from "@/components/JobListing.vue";
+import axios from "axios";
+import PulseLoader from "vue-spinner/src/PulseLoader.vue";
+
+const props = defineProps({
+	limit: {
+		type: String,
+		Required: true,
+	},
+	showButton: {
+		type: Boolean,
+		default: false,
+	},
+});
+
+const state = reactive({
+	jobs: [],
+	isLoading: true,
+});
+
+onMounted(async () => {
+	try {
+		const response = await axios.get("/api/jobs");
+
+		state.jobs = response.data;
+	} catch (error) {
+		console.error(error);
+	} finally {
+		state.isLoading = false;
+	}
+});
+</script>
+
 <template>
 	<section class="bg-green-50 px-4 py-10">
 		<div class="container-xl lg:container m-auto">
 			<h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
 				Browse Jobs
 			</h2>
-			<div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+
+			<!-- show loading spinner while loading is true -->
+			<div v-if="state.isLoading" class="text-center text-gray-500">
+				<PulseLoader />
+			</div>
+
+			<!-- show Jobs while done loading  -->
+			<div v-else class="grid grid-cols-1 md:grid-cols-3 gap-6">
 				<JobListing
-					v-for="job in jobs.slice(0, limit || jobs.length)"
+					v-for="job in state.jobs.slice(0, limit || state.jobs.length)"
 					:key="job.id"
 					:job="job"
 				/>
@@ -22,20 +65,3 @@
 		>
 	</section>
 </template>
-
-<script setup>
-import { ref, defineProps } from "vue";
-import { RouterLink } from "vue-router";
-import JobData from "@/jobs.json";
-import JobListing from "@/components/JobListing.vue";
-
-const jobs = ref(JobData.jobs);
-
-const props = defineProps({
-	limit: Number,
-	showButton: {
-		type: Boolean,
-		default: false,
-	},
-});
-</script>
