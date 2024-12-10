1be4fd10ee1b2bed2c86cec37a7d6bdb3607cd43
Ibrahim Bensaadoune <bensaadouneibrahim@gmail.com>
2024-12-10 20:42:28 +0100

    all done
    create add and edit job view and components with toast and spinner & edit Vite config for Axios

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,9 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import JobsView from "@/views/JobsView.vue";
 import JobView from "@/views/JobView.vue";
+import AddJobView from "@/views/AddJobView.vue";
 import NotFoundView from "@/views/NotFoundView.vue";
+import EditJobView from "@/views/EditJobView.vue";
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,6 +24,16 @@ const router = createRouter({
 			path: "/jobs/:id",
 			name: "job",
 			component: JobView,
+		},
+		{
+			path: "/jobs/add",
+			name: "add-job",
+			component: AddJobView,
+		},
+		{
+			path: "/jobs/edit/:id",
+			name: "job-edit",
+			component: EditJobView,
 		},
 		{
 			path: "/:catchAll(.*)",
